9622cdd56de2f5093faa6be897a4b8f75cfee14c
John Dukatz <jdukatz@email.arizona.edu>
2017-03-28 11:13:35 -0700

    some test commands

--- crud_test.sh
+++ crud_test.sh
@@ -2,7 +2,7 @@
 curl -X POST http://localhost:8080/api/movies -d '{"title": "Pirates of the Caribbean", "year": 2003, "producer": "Jerry Bruckheimer"}' -H "Content-Type: application/json"
 
 #read a record
-curl -X GET http://localhost:8080/api/movies/The-Shining
+curl -X GET http://localhost:8080/api/movies/Pirates-of-the-Caribbean
 
 #update the year for the shining
 curl -X PUT http://localhost:8080/api/movies/The-Shining -d '{"title": "The Shining", "year": 1980, "producer": "Stanley Kubrick"}' -H "Content-Type: application/json"
